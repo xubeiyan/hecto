5f9097d3a56613f4c3db9e07b90690da1daa55af
yangyangdi <1@1.cn>
2024-02-04 17:25:05 +0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+#![warn(clippy::all, clippy::pedantic, clippy::restriction)]
+#![allow(
+    clippy::missing_docs_in_private_items,
+    clippy::implicit_return,
+    clippy::shadow_reuse,
+    clippy::print_stdout,
+    clippy::wildcard_enum_match_arm,
+    clippy::else_if_without_else
+)]
+mod document;
+mod editor;
+mod terminal;
+mod row;
+mod highlighting;
+
+pub use document::Document;
+use editor::Editor;
+pub use editor::Position;
+pub use editor::SearchDirection;
+pub use row::Row;
+pub use terminal::Terminal;
+
+fn main() {
+    Editor::default().run();
+}
